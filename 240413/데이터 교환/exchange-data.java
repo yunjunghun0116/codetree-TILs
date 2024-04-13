dbdd633a9358902314ddf45f5a829181891106aa
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-04-13 21:00:22 +0900

    Title: 데이터 교환, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240413/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a= 5;
+        int b= 6;
+        int c= 7;
+        int temp = c;
+        c = a;
+        a = temp;
+
+        temp = c;
+        c = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
