14ef55a3339e443f78c65684ecf8252d5ded2b22
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-04-13 20:53:08 +0900

    Title: 두 실수의 곱, Time: 110ms, Memory: 9MB, Status: Passed - CodeTree

--- 240413/두 실수의 곱/the-product-of-two-real-numbers.java
+++ 240413/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -3,8 +3,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         double a = 5.26;
         double b=8.27;
-    double c = Math.round(a*b/1000)*1000;
+    double c = Math.round(a*b*1000)/1000.0;
     
-    System.out.println(String.format("%.3f", c));
+    System.out.println(String.format("%.3f",c));
     }
 }
