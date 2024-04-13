898dbafd81ab18e0a8bbfe033f48960d4c1d6484
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-04-13 20:42:01 +0900

    Title: 정수 선언하고 곱 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240413/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 26;
+
+        int b= 5;
+        System.out.println(a+" * "+b+" = "+a*b)
+    }
+}
