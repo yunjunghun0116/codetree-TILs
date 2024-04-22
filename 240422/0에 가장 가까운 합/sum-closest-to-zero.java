7054ba1b13adf26fe90f29a7a8790d5060e363a1
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-04-22 16:15:19 +0900

    Title: 0에 가장 가까운 합, Time: 543ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 240422/0에 가장 가까운 합/sum-closest-to-zero.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static int minValue;
+    public static void find(){
+        int end = arr.length-1;
+        Arrays.sort(arr);
+        minValue = Math.abs(arr[0]+arr[end]);
+        for(int i = 0;i < arr.length; i++){
+            while(i < end && Math.abs(arr[i]+arr[end]) < arr[i]+arr[end-1]){
+                minValue = Math.min(minValue,Math.abs(arr[i]+arr[end--]));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        arr = new int[Integer.parseInt(sc.nextLine())];
+        String[] input = sc.nextLine().split(" ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        find();
+
+        System.out.println(minValue);
+        
+    }
+}
