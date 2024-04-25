e4e3e67e018cb7e4c86c657a5843fac73ca4cee3
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-04-25 13:38:07 +0900

    Title: 서로 다른 k개의 문자, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240425/서로 다른 k개의 문자/k-distinct-characters.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int[] arr = new int[26];
+        Scanner sc = new Scanner(System.in);
+
+        String[] input = sc.nextLine().split(" ");
+
+        String str = input[0];
+        int diffCount = Integer.parseInt(input[1]);
+
+        int currentDiff = 0;
+
+        int maxLength = 0;
+        int end = 0;
+        for(int i = 0; i< str.length(); i++){
+            while(end < str.length() && currentDiff <= diffCount){
+                if(arr[getIndex(str.charAt(end))] == 0){
+                    currentDiff++;
+                }
+                arr[getIndex(str.charAt(end))] = arr[getIndex(str.charAt(end))]+1;
+                end++;
+            }
+            maxLength = Math.max(maxLength, end - i);
+            arr[getIndex(str.charAt(i))] = arr[getIndex(str.charAt(i))]-1;
+            if(arr[getIndex(str.charAt(i))] == 0){
+                currentDiff--;
+            }
+        }
+
+        System.out.println(maxLength);
+    }
+    public static int getIndex(char a){
+        return (int)a - (int)'a';
+    }
+}
