52277ac4adcab897c317edd0a1212868a6943f47
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-14 18:53:22 +0900

    Title: 화재 진압, Time: 451ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 240514/화재 진압/fire-suppression.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static int[] fires;
+    public static int[] stations;
+    public static int maxLength = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] sizeInput = sc.nextLine().split(" ");
+        int n = Integer.parseInt(sizeInput[0]);
+        int m = Integer.parseInt(sizeInput[1]);
+        fires = new int[n];
+        stations = new int[m];
+
+        String[] fireInput = sc.nextLine().split(" ");
+        for(int i = 0; i < n; i ++){
+            fires[i] = Integer.parseInt(fireInput[i]);
+        }
+        String[] stationInput = sc.nextLine().split(" ");
+        for(int i = 0; i < m; i ++){
+            stations[i] = Integer.parseInt(stationInput[i]);
+        }
+
+        Arrays.sort(fires);
+        Arrays.sort(stations);
+
+        find();
+
+        System.out.println(maxLength);
+    }
+
+    public static void find(){
+        int end = 0;
+        int preLength = Integer.MAX_VALUE;
+        for(int i = 0; i < fires.length; i++){
+            // i : 불의 위치
+            // end : 소방서 위치
+            preLength = Math.abs(stations[end] - fires[i]);
+            while(end < stations.length-1 && Math.abs(stations[end]-fires[i]) <= preLength){
+                preLength = Math.abs(stations[end]-fires[i]);
+                end++;
+            }
+            maxLength = Math.max(maxLength, preLength);
+        }
+    }
+}
