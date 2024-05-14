9650fd6950cdf54981812e8d842f6ade37261ec9
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-15 00:13:57 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 118ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240515/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+public class Main {
+    public static int minLength = Integer.MAX_VALUE;
+    public static int d;
+    public static Set<Integer> xSet = new HashSet<>();
+    public static List<Integer> list;
+
+    public static int[] minY = new int[1000001];
+    public static int[] maxY = new int[1000001];
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        d = Integer.parseInt(input[1]);
+
+        for(int i = 0; i < n; i++){
+            String[] point = sc.nextLine().split(" ");
+            int x = Integer.parseInt(point[0]);
+            int y = Integer.parseInt(point[1]);
+            xSet.add(x);
+
+            if(minY[x] == 0){
+                minY[x] = y;
+            }else{
+                minY[x] = Math.min(minY[x],y);
+            }
+
+            if(maxY[x] == 0){
+                maxY[x] = y;
+            }else{
+                maxY[x] = Math.min(maxY[x],y);
+            }
+        }
+
+        list = getXList();
+
+        int result = find();
+
+        System.out.println(result);
+    }
+
+    public static int find(){
+       
+        int end = 0;
+
+        int currMinY = minY[list.get(0)];
+        int currMaxY = minY[list.get(0)];
+
+        for(int i = 0; i < list.size(); i++){
+            while(end < list.size() && needFind(currMinY,currMaxY,list.get(end))){
+                currMinY = Math.min(currMinY,minY[list.get(end)]);
+                currMaxY = Math.max(currMaxY,maxY[list.get(end)]);
+                end++;
+            }
+            if(end == list.size()) break;
+            if(!needFind(currMinY,currMaxY,list.get(end))){
+                minLength = Math.min(minLength,Math.abs(list.get(end) - list.get(i)));
+                System.out.println("start : "+i+", end : "+end+", length : "+minLength);
+            }
+            if(minY[i] == currMinY){
+                currMinY = findMinValue(i,end);
+            }
+            if(maxY[i] == currMaxY){
+                currMaxY = findMaxValue(i,end);
+            }
+        }
+        if(minLength == Integer.MAX_VALUE) return -1;
+        return minLength;
+    }
+    public static int findMaxValue(int start,int end){
+        int maxValue = Integer.MIN_VALUE;
+        for(int i = start+1; i <= end; i++){
+            maxValue = Math.max(maxY[list.get(i)],maxValue);
+        }
+        return maxValue;
+    }
+    public static int findMinValue(int start,int end){
+        int minValue = Integer.MAX_VALUE;
+        for(int i = start+1; i <= end; i++){
+            minValue = Math.max(minY[list.get(i)],minValue);
+        }
+        return minValue;
+    }
+
+    public static List<Integer> getXList(){
+        List<Integer> xList = new ArrayList<>();
+        for(int i : xSet){
+            xList.add(i);
+        }
+
+        Collections.sort(xList);
+
+        return xList;
+    }
+    public static boolean needFind(int minValue,int maxValue,int x){
+        if(maxValue == Integer.MIN_VALUE || minValue == Integer.MAX_VALUE) return true;
+
+        if(Math.abs(minValue - maxY[x]) >= d) return false;
+        if(Math.abs(maxValue - minY[x]) >= d) return false;
+        return true;
+    }
+}
