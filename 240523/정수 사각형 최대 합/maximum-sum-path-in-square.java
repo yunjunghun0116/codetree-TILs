475b77423df99f615ea4455a817f64705cb7a787
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 13:41:20 +0900

    Title: 정수 사각형 최대 합, Time: 257ms, Memory: 14MB, Status: Passed - CodeTree

--- 240523/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 240523/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -26,13 +26,21 @@ public class Main {
         sum[0][0] = board[0][0];
     }
     public static void find(){
-        for(int i = 1; i < board.length; i++){
+        for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board.length; j++){
+                if(i==0 && j==0){
+                    sum[i][j] = board[i][j];
+                    continue;
+                }
+                if(i==0){
+                    sum[i][j] = sum[i][j-1]+board[i][j];
+                    continue;
+                }
                 if(j==0){
                     sum[i][j] = sum[i-1][j]+board[i][j];
                     continue;
                 }
-                sum[i][j] = Math.max(sum[i-1][j-1],sum[i][j-1])+board[i][j];
+                sum[i][j] = Math.max(sum[i-1][j],sum[i][j-1])+board[i][j];
             }
         }
     }
