9b6b49a3e39a97d6e4558efa862d5878589cfb2c
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 22:18:27 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1982ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240523/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240523/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -20,6 +20,12 @@ public class Main {
             }
         }
 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                maxBoard[i][j] = 1;
+            }
+        }
+
         find();
 
         System.out.println(maxCount);
@@ -28,31 +34,26 @@ public class Main {
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, -1, 0, 1};
 
-        Stack<int[]> positionStack = new Stack<>();
+        Queue<int[]> positionQueue = new LinkedList<>();
 
         for(int i = 0; i < n ; i++){
             for(int j = 0; j < n; j++){
-                if(canGo(i, j, 0, 0)){
-                    positionStack.push(new int[]{i, j, board[i][j], 1});
-                    while(!positionStack.isEmpty()){
-                        int[] position = positionStack.pop();
-                        maxCount = Math.max(maxCount, position[3]);
-                        for(int k = 0; k < 4; k++){
-                            int nextI = position[0] + dy[k];
-                            int nextJ = position[1] + dx[k];
-                            if(canGo(nextI, nextJ, position[2], position[3])){
-                                positionStack.push(new int[]{nextI, nextJ, board[nextI][nextJ], position[3]+1});
-                            } 
-                        }
+                if(maxBoard[i][j] != 1) continue;
+                positionQueue.add(new int[]{i, j, 1});
+                while(!positionQueue.isEmpty()){
+                    int[] position = positionQueue.poll();
+                    maxCount = Math.max(maxCount,position[2]);
+                    for(int k = 0; k < 4; k++){
+                        int nextI = position[0]+dy[k];
+                        int nextJ = position[1]+dx[k];
+                        if(position[2]+1 <= maxBoard[i][j]) continue;
+                        if(nextI < 0 || nextI >= n || nextJ < 0 || nextJ >= n) continue;
+                        if(board[nextI][nextJ] <= board[position[0]][position[1]]) continue;
+                        positionQueue.add(new int[]{nextI,nextJ,position[2]+1});
                     }
                 }
+                
             }
         }
-    }
-    public static boolean canGo(int i, int j, int value, int count){
-        if(i < 0 || i >= n || j < 0 || j >= n) return false;
-        if(count + 1 <= maxBoard[i][j]) return false;
-        if(board[i][j] <= value) return false;
-        return true;
     }
 }
