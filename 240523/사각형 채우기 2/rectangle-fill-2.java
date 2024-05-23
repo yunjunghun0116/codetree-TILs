a3cd7c1a673bd2a12030f6ab800e55c6a2d1db95
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 13:25:08 +0900

    Title: 사각형 채우기 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240523/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static int[] square = new int[1001];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        square[0] = 0;
+        square[1] = 1;
+        square[2] = 3;
+        for(int i =3; i < 1001; i++){
+            square[i] = square[i-1]+ square[i-2]*2;
+        }
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        System.out.println(square[n]);
+    }
+}
