14488f8d02d3c560d4c1f0862346fc07df4fa491
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 22:35:32 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 2961ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240523/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240523/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -5,6 +5,9 @@ public class Main {
     public static int[][] board;
     public static int[][] maxBoard;
     public static int maxCount = 0;
+
+    public static PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->board[a[0]][a[1]]-board[b[0]][b[1]]);
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -17,6 +20,7 @@ public class Main {
             String[] boardInput = sc.nextLine().split(" ");
             for(int j = 0; j < n; j++){
                 board[i][j] = Integer.parseInt(boardInput[j]);
+                pq.add(new int[]{i, j, 1});
             }
         }
 
@@ -27,27 +31,18 @@ public class Main {
     public static void find(){
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, -1, 0, 1};
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->board[a[0]][a[1]]-board[b[0]][b[1]]);
-
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < n; j++){
-                if(maxBoard[i][j] != 0) continue;
-                pq.add(new int[]{i, j, 1});
-                while(!pq.isEmpty()){
-                    int[] position = pq.poll();
-                    maxBoard[position[0]][position[1]] = position[2];
-                    maxCount = Math.max(maxCount,position[2]);
-                    for(int k = 0; k < 4; k++){
-                        int nextI = position[0]+dy[k];
-                        int nextJ = position[1]+dx[k];
-                        if(position[2]+1 <= maxBoard[i][j]) continue;
-                        if(nextI < 0 || nextI >= n || nextJ < 0 || nextJ >= n) continue;
-                        if(board[nextI][nextJ] <= board[position[0]][position[1]]) continue;
-                        pq.add(new int[]{nextI,nextJ,position[2]+1});
-                    }
-                }
-                
+        
+        while(!pq.isEmpty()){
+            int[] position = pq.poll();
+            maxBoard[position[0]][position[1]] = position[2];
+            maxCount = Math.max(maxCount,position[2]);
+            for(int k = 0; k < 4; k++){
+                int nextI = position[0]+dy[k];
+                int nextJ = position[1]+dx[k];
+                if(position[2]+1 <= maxBoard[position[0]][position[1]]) continue;
+                if(nextI < 0 || nextI >= n || nextJ < 0 || nextJ >= n) continue;
+                if(board[nextI][nextJ] <= board[position[0]][position[1]]) continue;
+                pq.add(new int[]{nextI,nextJ,position[2]+1});
             }
         }
     }
