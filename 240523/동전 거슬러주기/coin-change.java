dfab2ba25b47abb47ca38106adeb3b745b3e7484
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 23:55:20 +0900

    Title: 동전 거슬러주기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240523/동전 거슬러주기/coin-change.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static int coinCount;
+    public static int[] coins;
+    public static int findCoin;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] countInput = sc.nextLine().split(" ");
+        coinCount = Integer.parseInt(countInput[0]);
+        findCoin = Integer.parseInt(countInput[1]);
+        coins = new int[coinCount];
+
+        String[] coinInput = sc.nextLine().split(" ");
+        for(int i = 0; i < coinCount; i++){
+            coins[i] = Integer.parseInt(coinInput[i]);
+        }
+
+        int result = find();
+
+        System.out.println(result == 0 ? -1 : result);
+    }
+
+    public static int find(){
+        int[] dp = new int[findCoin+1];
+    
+        for(int i = 1; i <= findCoin; i++){
+            for(int j = 0; j < coinCount; j++){
+                if(i < coins[j]) continue;
+                if(dp[i] == 0){
+                    dp[i] = dp[i-coins[j]] + 1;
+                    continue;
+                }
+                dp[i] = Math.min(dp[i],dp[i - coins[j]]+1);
+                
+            }
+        }
+
+        return dp[findCoin];
+    }
+}
