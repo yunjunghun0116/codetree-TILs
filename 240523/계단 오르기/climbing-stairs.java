40aa806e2fdfa0811f8231a53a258db2411eeb17
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 12:07:36 +0900

    Title: 계단 오르기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240523/계단 오르기/climbing-stairs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static int[] stairs = new int[1001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        // 여기에 코드를 작성해주세요.
+        stairs[0] = 0;
+        stairs[1] = 0;
+        stairs[2] = 1;
+        stairs[3] = 1;
+        for(int i = 4; i < 1001; i++){
+            stairs[i] = stairs[i-3]+stairs[i-2];
+        }
+
+        System.out.println(stairs[n]%10007);
+    }
+}
