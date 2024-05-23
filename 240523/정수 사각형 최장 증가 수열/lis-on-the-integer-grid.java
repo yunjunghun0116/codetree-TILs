ce2a81aa5fd0a8430f9183b786845d835b3a2f54
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-23 22:25:06 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240523/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240523/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -7,14 +7,14 @@ public class Main {
     public static int maxCount = 0;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        n = Integer.parseInt(sc.nextLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
         board = new int[n][n];
         maxBoard = new int[n][n];
 
         for(int i = 0; i < n; i++){
-            String[] boardInput = sc.nextLine().split(" ");
+            String[] boardInput = br.readLine().split(" ");
             for(int j = 0; j < n; j++){
                 board[i][j] = Integer.parseInt(boardInput[j]);
             }
