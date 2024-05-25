fe62839ef61b20cee185e0ce24bc9aafa203f175
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-25 23:46:04 +0900

    Title: 배낭 채우기, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 240525/배낭 채우기/knapsack.java
+++ 240525/배낭 채우기/knapsack.java
@@ -12,7 +12,8 @@ public class Main {
         n = Integer.parseInt(sizeInput[0]);
         m = Integer.parseInt(sizeInput[1]);
 
-        dp = new int[n][m+1];
+        dp = new int[n+1][m+1];
+        jewelries.add(new int[]{});
         for(int i = 0; i < n; i++){
             String[] jewelry = sc.nextLine().split(" ");
             jewelries.add(new int[]{Integer.parseInt(jewelry[0]),Integer.parseInt(jewelry[1])});
@@ -20,15 +21,14 @@ public class Main {
 
         find();
 
-        System.out.println(dp[n-1][m]);
+        System.out.println(dp[n][m]);
     }
     public static void find(){
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             int[] jewelry = jewelries.get(i);
             for(int j = 1; j <= m; j++){
-                if(j < jewelry[0]) continue;
-                if(i == 0){
-                    dp[i][j] = jewelry[1];
+                if(j < jewelry[0]){
+                    dp[i][j] = dp[i-1][j];
                     continue;
                 }
                 dp[i][j] = Math.max(dp[i-1][j-jewelry[0]]+jewelry[1],dp[i-1][j]);
