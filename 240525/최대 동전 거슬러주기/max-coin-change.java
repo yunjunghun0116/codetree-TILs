56031826ee7fa2088b54f610149b914f0f71cb74
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-05-25 23:12:23 +0900

    Title: 최대 동전 거슬러주기, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240525/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[] dp;
+    public static int[] num;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        dp = new int[m+1];
+        num = new int[n];
+
+        String[] numInput = sc.nextLine().split(" ");
+        for(int i = 0; i < n ;i++){
+            num[i] = Integer.parseInt(numInput[i]);
+        }
+
+        find();
+
+        System.out.println(dp[m]==0?-1:dp[m]);
+    }
+    public static void find(){
+        for(int i = 1; i <= m; i++){
+            for(int j = 0; j < n; j++){
+                if(i < num[j]) continue;
+                dp[i] = Math.max(dp[i],dp[i-num[j]]+1);
+            }
+        }
+    }
+}
