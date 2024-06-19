b1c5db8fbef3f109876ae7b0350b44f0c1fa4b8e
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-06-19 16:25:17 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 149ms, Memory: 11MB, Status: Passed - CodeTree

--- 240619/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
+++ 240619/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -4,7 +4,8 @@ public class Main {
     public static int n;
     public static int m;
     public static int[][] board;
-    public static List<Integer> maxValueList = new ArrayList<>();
+    public static boolean[][] visited;
+    public static int maxResult = -1000;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         setUp();
@@ -14,9 +15,7 @@ public class Main {
             }
         }
 
-        Collections.sort(maxValueList,(a,b)->b-a);
-
-        System.out.println(maxValueList.get(0)+maxValueList.get(1));
+        System.out.println(maxResult);
     }
     public static void setUp(){
         Scanner sc = new Scanner(System.in);
@@ -33,22 +32,53 @@ public class Main {
             }
         }
     }
+    // a,b에서 m,n 개 만큼의 직사각형을 만듬
+    // 1에서 만든 후 만약 2에서 만든게 겹치면 바로 return, 다음으로
     public static void find(int y, int x){
-        int maxValue = Integer.MIN_VALUE;
         for(int i = 0; i < n-y; i++){
             for(int j = 0; j < m-x; j++){
-                maxValue = Math.max(maxValue,getSum(y,x,i,j));
+                visited = new boolean[n][m];
+                int sum = 0;
+                for(int p = 0; p <= i; p++){
+                    for(int q = 0; q<= j; q++){
+                        visited[y+p][x+q] = true;
+                        sum += board[y+p][x+q];
+                    }
+                }
+                int secondValue = findSecond();
+                maxResult = Math.max(maxResult,sum+secondValue);
             }
         }
-        maxValueList.add(maxValue);
     }
-    public static int getSum(int y, int x, int m, int n){
+    public static int findSecond(){
+        int maxSum = -1000;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < m; j++){
+                for(int p = 0; p < n-i; p++){
+                    for(int q = 0; q < m-j; q++){
+                        int[] secondValue = getSecondValue(i,j,p,q);
+                        if(secondValue[0]==1){
+                            maxSum = Math.max(maxSum,secondValue[1]);
+                        }
+                    }
+                }
+            }
+        }
+        return maxSum;
+    }
+    public static int[] getSecondValue(int y,int x,int p,int q){
+        boolean flag = true;
         int sum = 0;
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
+        for(int i = 0; i <= p; i++){
+            for(int j = 0; j <= q; j++){
+                if(visited[y+i][x+j]){
+                    flag = false;
+                    return new int[]{0,0};
+                }
                 sum += board[y+i][x+j];
             }
         }
-        return sum;
+        return new int[]{1,sum};
     }
+
 }
